94528187f741ca1a9ac5f4139099544a16b48223
richardejiang <richardejiang@hotmail.com>
2018-05-18 18:32:23 +0800

    modifies some return numbers

--- polls/getInsight.py
+++ polls/getInsight.py
@@ -43,15 +43,15 @@ def getAuthorInfo(inputFile):
 	
 
 	authors = [ele['name'] for ele in authorList if ele] # adding in the if ele in case of empty strings; same applies below
-	topAuthors = Counter(authors).most_common(5)
+	topAuthors = Counter(authors).most_common(10)
 	parsedResult['topAuthors'] = {'labels': [ele[0] for ele in topAuthors], 'data': [ele[1] for ele in topAuthors]}
 
 	countries = [ele['country'] for ele in authorList if ele]
-	topCountries = Counter(countries).most_common(5)
+	topCountries = Counter(countries).most_common(10)
 	parsedResult['topCountries'] = {'labels': [ele[0] for ele in topCountries], 'data': [ele[1] for ele in topCountries]}
 
 	affiliations = [ele['affiliation'] for ele in authorList if ele]
-	topAffiliations = Counter(affiliations).most_common(5)
+	topAffiliations = Counter(affiliations).most_common(10)
 	parsedResult['topAffiliations'] = {'labels': [ele[0] for ele in topAffiliations], 'data': [ele[1] for ele in topAffiliations]}
 
 	return {'infoType': 'author', 'infoData': parsedResult}

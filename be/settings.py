2d5cb36fd7f492c08dcf88d3661b5cd94df86e8b
richardejiang <richardejiang@hotmail.com>
2018-05-22 17:20:18 +0800

    currently cross origin allowing all

--- be/settings.py
+++ be/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', '0.0.0.0', 'chairviz.herokuapp.com']
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
